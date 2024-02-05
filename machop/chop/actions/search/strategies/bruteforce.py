130198b0dce287bfe15ebbe4ebe3d4f037adbff6
przemekff <p.forys2000@gmail.com>
2024-02-05 00:07:56 +0000

    add build the model inside bruteforce search

--- machop/chop/actions/search/strategies/bruteforce.py
+++ machop/chop/actions/search/strategies/bruteforce.py
@@ -4,9 +4,18 @@ class SearchStrategyBruteforce(SearchStrategyBase):
 
 
   def search(self, search_space):
-      sampled_config = search_space.flattened_indexes_to_config(search_space.choice_lengths_flattened)
+      #sampled_config = search_space.flattened_indexes_to_config(search_space.choice_lengths_flattened)
+      #print(search_space.choice_lengths_flattened)
+      #print(type(search_space.choice_lengths_flattened))
+      #mg = search_space.rebuild_model(sample)
+      #print(type(mg))
+      sampled_indexes = {}
+      #for name, length in search_space.choice_lengths_flattened.items():
+        #print(name)
+        #sampled_indexes[name] = 
+      sampled_config = search_space.flattened_indexes_to_config(sampled_indexes)
       mg = search_space.rebuild_model(sampled_config)
-      print(type(mg))
+
 
   def _post_init_setup(self) -> None:
     return None
